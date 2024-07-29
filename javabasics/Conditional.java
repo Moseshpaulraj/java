08b721bdbce3c27a9eb5383328e04a2f511f8ce4
Moseshpaulraj <132054313+Moseshpaulraj@users.noreply.github.com>
2024-07-29 19:01:53 +0530

    Delete javabasics directory

--- javabasics/Conditional.java
+++
@@ -1,9 +0,0 @@
-public class Conditional {
-    public static void main(String args[])
-    {
-        int a=45,b=35,c;
-        c=a>b?a:b;
-        System.out.println("The Greatest Number is : "+c);
- 
-    }
-}
--- javabasics/Logical.java
+++
@@ -1,9 +0,0 @@
-public class Logical {
-    public static void main(String args[])
-    {
-        int m1=10,m2=20;
-        System.out.println("And  &&  : "+(m1>=35 && m2>=35));
-        System.out.println("Or   ||  : "+(m1>=35 || m2>=35));
- 
-    }
-}
